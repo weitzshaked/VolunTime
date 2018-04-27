15069a7d68ac419ba0d9e87af737df1b8065fe54
weitzshaked <weitzshaked@gmail.com>
2018-04-27 06:01:59 +0300

    display

---
+++ app/src/main/java/il/co/hujihackathon/voluntime/Login1.java
@@ -0,0 +1,65 @@
+package il.co.hujihackathon.voluntime;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Login1 extends AppCompatActivity {
+    Button button1;
+    String msg = "Android = ";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login1);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        button1 = (Button) findViewById(R.id.button5);
+        button1.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+// TODO Auto-generated method stub
+
+                String mail = getmail();
+                String password = getPassword();
+                Intent androidsolved_intent1 = new Intent(getApplicationContext(), whoare.class);
+                startActivity(androidsolved_intent1);
+
+            }
+
+
+        });
+    }
+
+    public String getmail() {
+        TextView mail = (TextView) findViewById(R.id.editText4);
+        String value = mail.getText().toString();
+        Log.d(msg, value);
+        return value;
+    }
+
+    public  String getPassword(){
+        TextView password = (TextView) findViewById(R.id.editText5);
+        String value = password.getText().toString();
+        Log.d(msg, value);
+        return value;
+    }
+}
